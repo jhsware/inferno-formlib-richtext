91bd8ead3449d4de75248d479b9d14f04e41973b
jhsware <sebastian@urbantalk.se>
2018-11-08 14:25:06 +0100

    Cleanup of rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,74 +4,34 @@ import replace from 'rollup-plugin-replace';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-const baseConfig = (outputFormat) => {
-  const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-  let file;
-  switch (outputFormat) {
-    case 'cjs':
-      file = 'dist/index.' + outputFormat + (isProduction ? '.min' : '') + '.js';
-      break;
-
-    default:
-      throw new Error('Unsupported output format: ' + outputFormat);
-  }
-
-  return {
-    input: 'src/index.js',
-    plugins: [
-      nodeResolve({
-        modulesOnly: true, //
-      }),
-      commonjs({
-        include: 'node_modules/**'  // Default: undefined,
-      }),
-      babel({
-        runtimeHelpers: true
-      }),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      }),
-      isProduction ? minify({
-        comments: false,
-      }) : false,
-    ],
-    external: [
-      /* vvv These are from inferno-formlib vvv */
-      '@babel/runtime/helpers/createClass',
-      '@babel/runtime/helpers/classCallCheck',
-      '@babel/runtime/helpers/possibleConstructorReturn',
-      '@babel/runtime/helpers/getPrototypeOf',
-      '@babel/runtime/helpers/inherits',
-      '@babel/runtime/helpers/assertThisInitialized',
-      '@babel/runtime/helpers/typeof',
-      '@babel/runtime/helpers/objectWithoutProperties',
-      '@babel/runtime/helpers/defineProperty',
-      '@babel/runtime/helpers/objectSpread',
-      'classnames',
-      'lodash.tonumber',
-      'lodash.isobject',
-      /* ^^^ These are from inferno-formlib ^^^ */
-      'component-registry',
-      'inferno',
-      'inferno-formlib',
-      'isomorphic-schema'
-    ],
-    output: {
-      name: 'InfernoFormlibRichText',
-      file: file,
-      format: outputFormat,
-      sourcemap: true,
-      globals: {
-        'component-registry': 'componentRegistry',
-        'inferno': 'Inferno',
-        'inferno-formlib': 'infernoFormlib',
-        'isomorphic-schema': 'isomorphicSchema'
-      },
-    },
-  };
-};
-
-export default [
-  baseConfig('cjs')
-];
+export default {
+  input: 'src/index.js',
+  output: {
+    file: `dist/index.cjs${isProduction ? '.min' : ''}.js`,
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    babel({
+      runtimeHelpers: true
+    }),
+    isProduction ? minify({
+      comments: false,
+    }) : false
+  ],
+  external: [
+    'inferno',
+    'component-registry',
+    '@babel/runtime/helpers/createClass',
+    '@babel/runtime/helpers/classCallCheck',
+    '@babel/runtime/helpers/possibleConstructorReturn',
+    '@babel/runtime/helpers/getPrototypeOf',
+    '@babel/runtime/helpers/inherits',
+    '@babel/runtime/helpers/assertThisInitialized',
+    'isomorphic-schema',
+    'inferno-formlib',
+    'inferno-bootstrap'
+  ]
+}
